0cf6b449252bd58078a11438aaaae6d6453f2945
keshavunni <keshavunni37@gmail.com>
2023-06-24 20:50:36 +0530

    dummy push

--- test/test_triton.py
+++ test/test_triton.py
@@ -12,6 +12,7 @@ def program(b0, b1, b2):
 
 program_jit = JITFunction(program)
 
+
 # JITFunction(__main__:program) {'signature': {0: '*fp32', 1: '*fp32', 2: '*fp32'}, 'device': 0, 'constants': {}, 'num_warps': 4, 'num_stages': 3, 'extern_libs': None, 'configs': (instance_descriptor(divisible_by_16=(0, 1, 2), equal_to_1=()),)}
 # ast -> ttir -> ttgir -> llir -> ptx -> cubin
 compiled = compile(program_jit, signature={0: '*fp32', 1: '*fp32', 2: '*fp32'})
